e00fb5570c6c7c20836374cf935e770795e81ca0
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-26 08:01:45 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :candies do |t|
       t.string :name
-      t.integer :calories
+      t.integer :price
+      t.integer :size
+      t.string :image_url
       t.timestamps
     end
   end
